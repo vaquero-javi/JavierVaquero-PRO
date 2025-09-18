60d6663dbf81f1e6af34647d52920eb4582d19be
vaquero-javi <javiervaquerovila@gmail.com>
2025-09-18 11:28:46 +0200

    update

--- DAM2/PSP/Funcion/src/llamar.kt
+++ DAM2/PSP/Funcion/src/llamar.kt
@@ -1,3 +1,4 @@
 fun main(args:Array<String>){
     print("hola")
+    print("hola")
 }
--- DAM2/PSP/Funcion/src/uno.kt
+++ DAM2/PSP/Funcion/src/uno.kt
@@ -1,9 +1,10 @@
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-
-fun main(args:Array<String>){
 /*
+fun main(args:Array<String>){
+}
+
     //escribiendo /leyendo el error en el proceso principal
     val nombre = "arp -a"
     val proceso = ProcessBuilder("cmd.exe","/c","arp -a")
@@ -32,12 +33,15 @@ fun main(args:Array<String>){
     while (lector.readLine().also { linea = it } !=null){
         println(linea)
     }
+}
 */
 
-
-    var linea = ProcessBuilder("C:\\Users\\javie\\.jdks\\openjdk-24.0.1\\bin\\java.exe \"-javaagent:C:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2024.3.5\\lib\\idea_rt.jar=54983\" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\\Users\\javie\\OneDrive\\Documentos\\GitHub\\JavierVaquero-PRO\\DAM2\\PSP\\Funcion\\out\\production\\Funcion;C:\\Users\\javie\\.m2\\repository\\org\\jetbrains\\kotlin\\kotlin-stdlib\\2.1.10\\kotlin-stdlib-2.1.10.jar;C:\\Users\\javie\\.m2\\repository\\org\\jetbrains\\annotations\\13.0\\annotations-13.0.jar LlamarKt").start().inputStream)).readLine()
-    println(linea)
+fun main (args: Array<String>){
+    var linea = BufferedReader (InputStreamReader(ProcessBuilder("C:\\Users\\javie\\.jdks\\openjdk-24.0.1\\bin\\java.exe\" \"-javaagent:C:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2024.3.5\\lib\\idea_rt.jar=49855\" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\\Users\\javie\\OneDrive\\Documentos\\GitHub\\JavierVaquero-PRO\\DAM2\\PSP\\Funcion\\out\\production\\Funcion;C:\\Users\\javie\\.m2\\repository\\org\\jetbrains\\kotlin\\kotlin-stdlib\\2.1.10\\kotlin-stdlib-2.1.10.jar;C:\\Users\\javie\\.m2\\repository\\org\\jetbrains\\annotations\\13.0\\annotations-13.0.jar LlamarKt").start().inputStream)).readLine()
+    println(linea.uppercase())
 }
+
+
 
 
 
