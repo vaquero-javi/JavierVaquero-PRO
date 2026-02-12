b994340b2a163c5e435a71f1ee1353e1f35e6977
vaquero-javi <javiervaquerovila@gmail.com>
2026-02-12 12:41:39 +0100

    update

--- DAM2/DI/TEMA 5/tareas/src/app/app.ts
+++ DAM2/DI/TEMA 5/tareas/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('tareas');
-opcion: any;
+  protected readonly title = signal('todolist');
+  opcion: string = '1';
+
+  constructor(private gestorRutas: Router) {}
+
+  navegar(parm: string) {
+    // cuando tenga algun procesamiento de datos
+    this.gestorRutas.navigate(['buscar', parm]);
+  }
 }
